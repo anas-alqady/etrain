10e62ccc3ee65ee16455abb7e44a141cf7e43e2a
Ashraf <suha.hijawi@roya.tv>
2020-02-15 22:29:24 +0200

    project2

--- include/config.php
+++ include/config.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$mysql = new mysqli('localhost','root','','hemma');
+$mysql = new mysqli('localhost','id12598945_hemma','ans0172890','id12598945_hemma');
 $mysql->set_charset('utf8');
 
 
